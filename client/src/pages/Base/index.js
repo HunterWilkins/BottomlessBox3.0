1155ebd4fa6e3e53460ae6b9ed8e253025977460
HunterWilkins <wilkins.hunter@gmail.com>
2020-10-30 19:02:07 -0500

    Got Pocket Selection and Item Filtering Implemented

--- client/src/pages/Base/index.js
+++ client/src/pages/Base/index.js
@@ -5,7 +5,8 @@ import "./style.css";
 class Base extends Component {
     state = {
         pockets: [{name: "All"}],
-        items: []
+        items: [],
+        currentPocket: undefined
     }
 
     componentDidMount = () => {
@@ -17,7 +18,12 @@ class Base extends Component {
         const {data} = await axios.get("/api/pockets/filtered");
 
         this.setState({pockets: [...this.state.pockets, ...data]});
-        console.log(this.state);
+        console.log(this.state.pockets);
+    }
+
+    getCurrentPocketName = () => {
+        let currentPocketName = this.state.pockets.filter(pocket => pocket.pocket_id === this.state.currentPocket)[0].name;
+        return currentPocketName;
     }
     
     getItems = async () => {
@@ -29,11 +35,35 @@ class Base extends Component {
         axios.post("/api/item", data);
     }
 
+    changePocket = (pocketId) => {
+        console.log("Change pocket to... " + pocketId);
+        this.setState({
+            currentPocket: pocketId
+        });
+    }
+
     renderPockets = () => {
         return this.state.pockets.map(pocket => 
-            <img className = "pocket" src = {"./images/" + pocket.name  + ".png"} alt = {pocket.name}></img>
+            <img 
+                className = "pocket" 
+                src = {"./images/" + pocket.name  + ".png"} 
+                alt = {pocket.name}
+                onClick = {() => this.changePocket(pocket.pocket_id)}>
+            </img>
         );
     };
+
+    renderItems = () => {
+        const filteredItems = this.state.items.filter(item => item.pocket_id === this.state.currentPocket);
+
+        console.log(filteredItems);
+        if (this.state.currentPocket === undefined) {
+            return this.state.items.map(item => <p>{item.name}</p>)
+        }
+        else {
+            return filteredItems.map(item =><p>{item.name}</p>);
+        }
+    }
 
     render() {
         return(
@@ -41,19 +71,11 @@ class Base extends Component {
                 <nav>
                     {this.renderPockets()}
                 </nav>
-                <h1>Hello!</h1>
-                <section>
-
+                <h1>{this.getCurrentPocketName()}</h1>
+                <section id = "item-box">
+                    {this.renderItems()}
                 </section>
-                <button onClick = {() => {
-                    this.createItem({
-                        name: "Random Food Item",
-                        user_id: 1,
-                        price: 12.75,
-                        quantity: 1,
-                        pocket_id: 5
-                    });
-                }}>Add Item</button>
+               
                 
             </main>
         )
